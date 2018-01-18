615b6b95966dcffe27f9e6dd4531e990b56fb29b
sanioknur <35558142+sanioknur@users.noreply.github.com>
2018-01-18 02:57:35 -0500

    Add files via upload

---
+++ EFSreport.ps1
@@ -0,0 +1,11 @@
+﻿$crypt=Cipher /U /N
+$arr = @()
+gci $crypt -recurse | ? {$_.PSIsContainer -eq $False} | % {
+  $obj = New-Object PSObject
+  $obj | Add-Member NoteProperty Directory $_.DirectoryName
+  $obj | Add-Member NoteProperty Name $_.Name
+  $obj | Add-Member NoteProperty Length $_.Length
+  $obj | Add-Member NoteProperty Owner ((Get-ACL $_.FullName).Owner)
+  $arr += $obj
+  }
+  $arr | Export-CSV -notypeinformation "report.csv"
